684e3bcd2d327990610c3b72e25a1fd1b8988bfb
bri-ne <cervantesblb@gmail.com>
2024-08-21 22:25:41 -0400

    getting csv outputs
    added dependency for FS
    
    searchApiCall
    //--- editing this to put results into a csv instead
    // changed the parameters of this function to allow for easy access of company name later on

--- Mineral-Search.js
+++ Mineral-Search.js
@@ -21,6 +21,8 @@ require('dotenv').config();
 const cheerio           = require('cheerio');
 const request           = require('request');
 const axios             = require('axios');
+const fs                = require('fs'); 
+const { convertArrayToCSV } = require('convert-array-to-csv');
 const keywords          = require('./mineral-keywords.json');
 const auditorList       = keywords.auditors;
 const mineralList       = keywords.mineralsList;
@@ -35,7 +37,8 @@ var token           = process.env.SEC_API_KEY;
 
 
 var testPostURL     = `https://api.sec-api.io/full-text-search?token=${token}`;
-
+//rows is an array of arrays that will be turned into a csv 
+var rowObjects = []; 
 
 
 
@@ -43,21 +46,28 @@ var testPostURL     = `https://api.sec-api.io/full-text-search?token=${token}`;
 //TODO: get company name to search
 //TODO: get Date specified
 
+// STARTING WITH JUST A SMALL TIME FRAME
 //Dates: "YYYY-MM-DD" format
-let startDateInput = "2005-01-01";
-let stopDateInput = "2024-07-31";
+// Se
+let startDateInput = "2023-01-01";
+let stopDateInput = "2023-05-31";
+let ck = 1
 
 
 
 //--- Testing results for a specific company and dates: ---
-console.log("--- "+companyList.at(2)+" ---");
-searchApiCall(companyList.at(2)+allMinerals, startDateInput, stopDateInput);
-
+console.log("--- "+companyList.at(ck)+" ---");
+searchApiCall(companyList.at(ck),allMinerals, startDateInput, stopDateInput);
+//--- this will be mroe useful once the search api fn loops through multiple searches 
+produceCSV(rowObjects);
 
 
 
 //--- Searches SEC EDGAR results using Api to obtain relevant filings ---
-function searchApiCall(searchString, startDateString, stopDateString) {
+//--- editing this to put results into a csv instead
+// changed the parameters of this function to allow for easy access of company name later on
+function searchApiCall(company,minerals,startDateString, stopDateString) { 
+    let searchString = company + minerals 
     console.log(searchString)
 
     axios.post(testPostURL,   {
@@ -78,11 +88,13 @@ function searchApiCall(searchString, startDateString, stopDateString) {
                 postRes = response.data.filings;
                 //List of specific filing URLS (to be filled)
                 var urlList = [];
+                var filingyearList = [];
+
 
                 //testing
                 //console.log(postRes[0].cik);
-                console.log('thisis the lenght of postres')
-                console.log(postRes.length);
+                //console.log('thisis the lenght of postres')
+                //console.log(postRes.length);
  
                 for(let i = 0; i < postRes.length; i++)
                 {
@@ -90,20 +102,21 @@ function searchApiCall(searchString, startDateString, stopDateString) {
                     //console.log('index = '+i);
                     //console.log(postRes[i]);
                     urlList.push(String(postRes[i].filingUrl));
+                    filingyearList.push(String(postRes[i].filedAt)); 
                 }
                 console.log('url list:')
                 console.log(urlList);             
-                    let element = urlList[4];
+                    let element = urlList[0];
+                    let filingYear = filingyearList[0];
                     //testing
-                    console.log('this is ELEMENT')
-                    console.log(element);
+                    //console.log('this is ELEMENT')
+                    //console.log(element);
     
-                     
                     let idurl = element.replace("https://www.sec.gov/Archives/edgar/data/", "");
                     //testing
-                    //console.log(idurl);
+                    console.log(idurl);
     
-                    let testGetURL      = `https://archive.sec-api.io/${idurl}?token=${token}&type=${type}`;
+                    let testGetURL = `https://archive.sec-api.io/${idurl}?token=${token}&type=${type}`;
     
                     console.log(testGetURL)
                      axios.get(testGetURL)
@@ -119,6 +132,16 @@ function searchApiCall(searchString, startDateString, stopDateString) {
                          var knownMinerals = parseDoc(data, mineralList);
                          var knownTableMinerals = parseDoc(data, tableMineralList);
                          
+                         //single row of table to be made in this order
+                        ///var row = [company, filingYear, knownTableMinerals, knownMinerals, knownAuditors,testGetURL]
+                         rowObjects.push({companyName:company,
+                            filingDate: filingYear, 
+                            tableMineralArray: knownTableMinerals, 
+                            MineralsArray: knownMinerals, 
+                            Auditors: knownAuditors,
+                            filingUrl: testGetURL})
+
+                         console.log(rowObjects)
                          console.log("  Known Auditors:");
                          knownAuditors.forEach(element => {
                              console.log("      "+element);
@@ -133,16 +156,14 @@ function searchApiCall(searchString, startDateString, stopDateString) {
                          knownTableMinerals.forEach(element => {
                             console.log("       "+element);
                         });
-                         
-                 });
+                        
+                 }).then(result=> console.log('Inside callback!'));//hoping this will help 
     
                 //save data to return
             }
         
 
         });
-       
-
 }
 
 //--- Parses individual filing for our search terms ---
@@ -169,3 +190,16 @@ function parseDoc(docFullText, keywordList){
 
 }
 
+//convert rowobjects to csv and output a file
+function produceCSV(objects){
+    // this will be mroe useful once the search api fn loops through multiple searches 
+    var csvFromArrayOfObjects = convertArrayToCSV(objects);
+    console.log("this is csvFromArrayOfObjects")
+    console.log(csvFromArrayOfObjects)
+    fs.writeFile('output.csv', csvFromArrayOfObjects, err=> {
+      if (err) {
+          console.log(18, err);
+      }
+      console.log("CSV file saved succesfully!")
+    })
+}
