cc95352fda162c138128784dbb88360b0e668ec9
bri-ne <cervantesblb@gmail.com>
2024-09-28 15:56:53 -0400

    update on companies

--- pythonOption/callingFunctions.py
+++ pythonOption/callingFunctions.py
@@ -4,8 +4,7 @@ mineralKeyword = {
     "tableMineralsList" : [">Cobalt<",">Tin<",">Lithium<", ">Gold<", ">Copper<", ">Gallium<", ">Aluminum<", ">Nickel<", ">Silicon<", ">Germanium<"],
     "mineralsList"      : ["Cobalt", "Lithium", "Gold", "Copper", "Gallium", "Aluminum", "Nickel", "Silicon", "Germanium"],
     "mineralsString"    : " \"Cobalt\" OR \"Lithium\" OR \"Gold\" OR \"Copper\" OR \"Gallium\" OR \"Aluminum\" OR \"Nickel\" OR \"Silicon\" OR \"Germanium\" ",
-    "companies"         : ["Tesla","Microsoft", "Micron"],
-    "cik"               : ["0001318605", "0000789019", "0000723125", "0000006281", "0001604778", "0000804328", "0000707549"],
+    "companies"         : ["Applied Materials","Advanced Semiconductor Engineering","Dell","Global Foundries","Google","Alphabet","Intel","KLA Corporation","EMD Electronics","Polar Semiconductor","TSMC","Microsoft","Apple","Tesla","Sony Group Corp","Marvell","Micron Technology","Seagate Technology Holdings"],
     "auditors"          : ["Responsible Minerals Initiative", "RMI", "Responsible Business Alliance", "RBA", "Fair Trade"],
     "formTypes"         : ["SD"]
 }
